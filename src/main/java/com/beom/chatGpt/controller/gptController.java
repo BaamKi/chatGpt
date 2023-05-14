919fb1daa59409b04dadd17d575f2dffb11517b8
baamki <sevenbumki@gmail.com>
2023-05-14 12:46:30 +0900

    ChatGPT

--- src/main/java/com/beom/chatGpt/controller/gptController.java
+++ src/main/java/com/beom/chatGpt/controller/gptController.java
@@ -4,10 +4,7 @@ import com.beom.chatGpt.service.ChatService;
 import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +19,12 @@ public class gptController {
     public String test(@RequestBody String question){
         return chatService.getChatResponse(question);
         //\n\nAs an AI language model, I don't have feelings, but I'm functioning well. Thank you for asking. How can I assist you today?
+    }
+
+    @GetMapping("/api/healthcheck")
+    public String healthCheck()
+    {
+        return "healthCheck";
     }
 
 }
